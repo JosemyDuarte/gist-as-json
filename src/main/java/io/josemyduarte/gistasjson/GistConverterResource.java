446607927f1c5741c26d448d706c7f7e843dd2b2
Josemy Duarte <duartejosemy@gmail.com>
2020-07-04 00:06:13 +0200

    Added exception handling for invalid Json

--- src/main/java/io/josemyduarte/gistasjson/GistConverterResource.java
+++ src/main/java/io/josemyduarte/gistasjson/GistConverterResource.java
@@ -15,16 +15,23 @@ import java.net.URI;
 @Path("/asJson")
 public class GistConverterResource {
 
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
     @GET
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
-    public JsonNode convert(@QueryParam("url") String url) throws JsonProcessingException {
+    public JsonNode convert(@QueryParam("url") String url) {
 
         String myResume = RestClientBuilder.newBuilder()
                 .baseUri(URI.create(url))
                 .build(GistClient.class)
                 .getMyResume();
 
-        return new ObjectMapper().readValue(myResume, JsonNode.class);
+        try {
+            return OBJECT_MAPPER.readValue(myResume, JsonNode.class);
+        } catch (JsonProcessingException e) {
+            return OBJECT_MAPPER.createObjectNode()
+                    .put("message", "We had a problem converting your gist to JSON. Are you sure is a valid JSON?");
+        }
     }
 }
--- src/test/java/io/josemyduarte/gistasjson/GistConverterResourceTest.java
+++ src/test/java/io/josemyduarte/gistasjson/GistConverterResourceTest.java
@@ -10,11 +10,21 @@ import static org.hamcrest.CoreMatchers.*;
 public class GistConverterResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testAsJsonEndpoint() {
         given()
                 .when().get("/asJson?url=https://gist.githubusercontent.com/JosemyDuarte/7e05cad1dadfec4eb2af44e9a66f8f39/raw/d64c278204414faa9b0f3873e1b2dc66a0bd5931/resume.json")
                 .then()
                 .statusCode(200)
                 .body(is(not(nullValue())));
     }
+
+    @Test
+    public void testAsJsonEndpointWithInvalidJson() {
+        given()
+                .when().get("/asJson?url=https://gist.githubusercontent.com/JosemyDuarte/7892331a75bf3412af78f0df1b82bf07/raw/169c97bfca829013d8076ba574bcaef18ada37b5/search.py")
+                .then()
+                .statusCode(200)
+                .body("message", is(not(nullValue())));
+    }
+
 }
